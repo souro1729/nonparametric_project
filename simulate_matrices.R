69911eff2ab77cf5f3101d0aa76d0d43ebc800e9
souro1729 <61871902+souro1729@users.noreply.github.com>
2020-03-21 17:53:32 +0530

    Update simulate_matrices.R

--- simulate_matrices.R
+++ simulate_matrices.R
@@ -1,7 +1,7 @@
 ## This file contains code for different simulations and storing them into matrices
 
 #short function to convert integers into string
-c <- function(x) as.character(x)
+st <- function(x) as.character(x)
 
 #returns rejection probability using F statistic
 parametric_rejection = function(n, m, 
@@ -27,27 +27,48 @@ find_rejection_matrix <- function(nvec, mvec,
                                   rdist = rNorm, theta = 1, 
                                   repl = 1000, p = 0.05)
 {
+  #progress bar
+  pb <- txtProgressBar(style = 3, min = 1, max = length(mvec) * length(nvec), initial = 1)
+  k <- 1
+  #rejection probability
   reject_prob = matrix(0,length(mvec),length(nvec))
   rownames(reject_prob) <- nvec; colnames(reject_prob) <- mvec; 
-  sapply(nvec, function(i){
-    sapply(mvec, function(j){
-      reject_prob[c(i), c(j)] = stat_rejection(i, j, rdist, theta, parameter, repl, p)
-    })
-  })
+  for(i in nvec){
+    for(j in mvec){
+      reject_prob[st(i), st(j)] = stat_rejection(i, j, rdist, theta, repl, p)
+      k <- k + 1 #pb
+      setTxtProgressBar(pb, k) #pb
+    }
+  }
+  close(pb) #pb
+  write.csv(reject_prob, sprintf("data/%s -- theta=%.3f -- %s -- n:%s -- m:%s.csv", 
+                                 as.character(substitute(stat_rejection)),
+                                 theta, as.character(substitute(rdist)), 
+                                 paste(nvec, collapse = ", "), paste(mvec, collapse = ", ")))
   reject_prob
 }
 
-normality_matrix <- function(nvec, mvec, 
+normality_matrix <- function(nvec, mvec, repl=1000,
                              simulate_stat= simulate_capon, #change this input to get results of different statistics
                              p = 0.05)
 {
-  testmat = matrix(0,length(nvec),length(mvec))
+  #progress bar
+  pb <- txtProgressBar(style = 3, min = 1, max = length(mvec) * length(nvec), initial = 1)
+  k <- 1
+  #shapiro wilk test matrix
+  testmat = matrix(0, length(nvec), length(mvec))
   rownames(testmat) <- nvec; colnames(testmat) <- mvec; 
-  sapply(nvec, function(i){
-    sapply(mvec, function(j){
+  for(i in nvec){
+    for(j in mvec){
       data = replicate(repl, simulate_stat(i, j, theta = 1))
-      testmat[c(i), c(j)] = shapiro.test(data)$p.value
-    })
-  })
+      testmat[st(i), st(j)] = shapiro.test(data)$p.value
+      k <- k + 1 #pb
+      setTxtProgressBar(pb, k) #pb
+    }
+  }
+  close(pb) #pb
+  write.csv(testmat, sprintf("data/%s -- %s.csv", 
+                             as.character(substitute(simulate_stat)),
+                             paste(nvec, collapse = ", ")))
   testmat
 }
