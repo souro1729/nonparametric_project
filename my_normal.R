961881aeaf83bf80f444a51f85f39de2e096ea61
souro1729 <61871902+souro1729@users.noreply.github.com>
2020-03-22 22:52:13 +0530

    Update my_normal.R

--- my_normal.R
+++ my_normal.R
@@ -1,19 +1,19 @@
-theta=seq(1,1.3,len=1000)
+theta=seq(1,2,len=1000)
 
 
-parametric_rejection = function(m, n, 
-                                rdist = rnorm, theta=1,
+parametric_rejection_norm = function(m, n, 
+                                 theta=1,
                                 repl = 1000, p = 0.05) #total replication, and level
 {
   new=function(){
     x = rnorm(m,0,1)
-    y = rnorm(n, 1,theta)
+    y = rnorm(n,0,theta)
     var.test(x, y, alternative = "less")$p.value
   }
   s=replicate(repl,new())
   length(which(s<p))/repl
 }
 
-c=sapply(theta,parametric_rejection,m=1000,n=1000,repl=10000,p=0.001,rdist=rnorm)
+c=sapply(theta,parametric_rejection,m=1000,n=1000,repl=10000,p=0.05)
 library(lattice)
 xyplot(c~theta,type="l")
