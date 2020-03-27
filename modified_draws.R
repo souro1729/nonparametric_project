ccbdbe99d20840cb84fe8bbef2542869d457b06c
souro1729 <61871902+souro1729@users.noreply.github.com>
2020-03-28 03:15:12 +0530

    Update modified_draws.R

--- modified_draws.R
+++ modified_draws.R
@@ -3,14 +3,18 @@ rExp <- function(n, theta) {
   rexp(n, rate = 1/theta)
 }
 
-rGamma <- function(n, theta, shape=5) {
+rGamma <- function(n, theta, shape=9) {
   rgamma(n, shape = shape, scale = theta)
 }
 
-rWeibull <- function(n, theta, shape=5){
+rWeibull <- function(n, theta, shape=9){
   rweibull(n, shape = shape, scale = theta)
 }
 
 rNorm <- function(n, theta=1) {
   rnorm(n, mean = 0, sd = theta)
+}
+
+rLogis<-function(n,theta){
+  rlogis(n,scale = theta)
 }
