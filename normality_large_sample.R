82dcc762ac135bee7c51d3097878cda9f264245a
souro1729 <61871902+souro1729@users.noreply.github.com>
2020-04-10 20:51:00 +0530

    Update normality_large_sample.R

--- normality_large_sample.R
+++ normality_large_sample.R
@@ -1,17 +1,17 @@
-c=seq(10,30,1)
-Chii=function(n,m=40){
-  replicate(n,(rchisq(1,m)-m)/sqrt(2*m))
-  #sum(s)/n
+
+Chii=function(n,m=40,breaking=20){
+  library(zoo)
+  x=replicate(n,(rchisq(1,m)-m)/sqrt(2*m))
+  p1=hist(x,breaks = breaking)
+  qplot=c(qqnorm(x),qqline(x))
+  shap_test=shapiro.test(x)
+  breaks_cdf <- pnorm(p1$breaks)
+  null.probs <- rollapply(breaks_cdf, 2, function(x) x[2]-x[1])
+  chi_test <- chisq.test(p1$counts, p=null.probs, rescale.p=TRUE, simulate.p.value=TRUE)
+  my_list=list("hist"=p1,"qqplot"=qplot,"shapiro_test"=shap_test,"chisq.test"=chi_test)
+  return(my_list)
 }
-
-library(pbapply)
-d=Chii(n=40)
-
-#dd=rnorm(100)
-qqnorm(d)
-qqline(d)
-shapiro.test(d)
-d1=Chii(n=15,m=10)
-qqnorm(d1)
-qqline(d1)
-shapiro.test(d1)
+a=Chii(n=50,m=50,breaking = 25)
+a1=Chii(n=50,m=10,breaking = 25)
+a1
+a
