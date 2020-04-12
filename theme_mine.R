e8b8873943e17cd254869f72d1985f8e55212c8e
souro1729 <61871902+souro1729@users.noreply.github.com>
2020-04-12 17:27:53 +0530

    Update theme_mine.R

--- theme_mine.R
+++ theme_mine.R
@@ -2,21 +2,19 @@ theme_mine <- function (base_size = 12, base_family = "") {
   require(grid)
   theme(line = element_line(colour = "black", size = 0.5, linetype = 1, lineend = "butt"),
         rect = element_rect(fill = "white", colour = "black", size = 0.5, linetype = 1),
-        text = element_text(family = base_family,face = "plain", colour = "green",
-                            size = base_size, hjust = 0.5, angle = 0, lineheight = 0.1),
-        axis.text = element_text(size = rel(0.8)),
-        strip.text = element_text(size = rel(0.8)),
+        text = element_text(family = base_family,face = "plain", colour = "black",
+                            size = base_size, hjust = 0.5,vjust = 0.5, angle = 0, lineheight = 0.9),
+        axis.text = element_text(size = rel(0.8),margin = margin(0.1,0.1,0.1,0.1, "cm")),
         axis.line = element_blank(),
-        axis.text.x = element_text(vjust = 1,colour = "black"),
+        axis.text.x = element_text(vjust = 1,colour = "black",margin=margin(0.1,0.1,0.1,0.1,"cm") ),
         axis.text.y = element_text(hjust = 1,colour="black"),
-        axis.ticks = element_line(colour = "grey50"),
         axis.title.x = element_text(colour = "black"),
         axis.title.y = element_text(angle = 90,colour = "black"),
         axis.ticks.length = unit(0.15, "cm"),
-        axis.ticks.margin = unit(0.1, "cm"),
+        #axis.ticks.margin = margin(0.1,0.1,0.1,0.1, "cm"),
         
         legend.background = element_rect(colour = NA),
-        legend.margin = unit(0.2, "cm"),
+        legend.margin = margin(0.2,0.1,0.2,0.2, "cm"),
         legend.key = element_rect(colour = "grey80"),
         legend.key.size = unit(1.2, "lines"),
         legend.key.height = NULL,
@@ -36,13 +34,14 @@ theme_mine <- function (base_size = 12, base_family = "") {
         panel.grid.minor = element_line(colour = "grey98", size = 0.5),
         panel.spacing = unit(0.25, "lines"),
         
-        strip.background = element_rect(fill = "grey80", colour = "grey50"),
+        strip.text = element_text(size = rel(0.8)),
+        strip.background = element_rect(fill = "#fcac59", colour = "grey50"),
         strip.text.x = element_text(),
         strip.text.y = element_text(angle = -90),
         
         plot.background = element_rect(colour = "white"),
-        plot.title = element_text(size = rel(1.2)),
-        plot.margin = unit(c(1, 1, 0.5, 0.5), "lines"), complete = TRUE)
+        plot.title = element_text(size = rel(1.2),face = "bold",colour="brown",margin = margin(0.25,0.25,0.25,0.25,"lines")),
+        plot.margin = unit(c(1, 1, 0.8, 0.5), "lines"),complete = TRUE)
 }
 
 
