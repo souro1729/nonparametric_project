235e39fc872db2f43c46ba46a8ea90cf57f05291
souro1729 <61871902+souro1729@users.noreply.github.com>
2020-04-13 16:55:18 +0530

    Update Untitled.R

--- Untitled.R
+++ Untitled.R
@@ -4,10 +4,17 @@ print("Run this Code....")
 
 
 Dirname=getwd()
+Dirname
+subDir <- "Group-8"
+file=file.path(Dirname,subDir)
+dir.create(file, showWarnings = FALSE)
+setwd(file.path(Dirname, subDir))
 
-subDir <- ""
-dir.create(file.path(mainDir,subDir), showWarnings = FALSE)
-setwd(file.path(mainDir, subDir))
+
+download.file("https://github.com/souro1729/nonparametric_project/souro.zip",file.path(Dirname, subDir))
+
+dir.create("New")
+unzip()
 
 
 
