c1f6867c81181b7d4059031309f9fdf93fdb08da
souro1729 <61871902+souro1729@users.noreply.github.com>
2020-04-13 20:11:56 +0530

    Update main.R

--- main.R
+++ main.R
@@ -11,7 +11,7 @@ dir.create(file, showWarnings = TRUE)
 setwd(file.path(Dirname, subDir))
 dir.create("New")
 
-download.file("https://github.com/souro1729/nonparametric_project/raw/master/souro.zip",destfile = "souro.zip")
+download.file("https://github.com/souro1729/nonparametric_project/raw/master/Souro.zip",destfile = "Souro.zip")
 unzip("souro.zip",exdir = file)
 
 
