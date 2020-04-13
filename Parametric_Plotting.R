6e28c207e1aab94f3f30ee96a64c0f2a9270ac7e
souro1729 <61871902+souro1729@users.noreply.github.com>
2020-04-13 21:20:54 +0530

    Update Parametric_Plotting.R

--- Parametric_Plotting.R
+++ Parametric_Plotting.R
@@ -24,8 +24,8 @@ L_Gamma<-as.vector(read.csv(file.path(Dir_name,"power -- parametric_rejection (5
 L_Exp<-as.vector(read.csv(file.path(Dir_name,"power -- parametric_rejection (50, 50) -- rExp -- 0.20, 1.00, 1000.00 -- repl=10000.csv")))[,-1]
 L_Logis<-as.vector(read.csv(file.path(Dir_name,"power -- parametric_rejection (50, 50) -- rLogis -- 0.20, 1.00, 1000.00 -- repl=10000.csv")))[,-1]
 
-theta=seq(0.1,1,len=1000)
-theta2=seq(0.2,1,len=1000)
+theta=seq(10,1,len=1000)
+theta2=seq(5,1,len=1000)
 
 data=data.frame(cbind(L_Normal,L_Gamma,L_Exp,L_Logis,L_Weibull,theta2))
 data1=data.frame(cbind(S_Normal,S_Gamma,S_Exp,S_Logis,S_Weibull,theta))
@@ -39,11 +39,11 @@ econdata <- gather(data, key="measure", value="value", c("L_Normal","L_Gamma","L
 
 
 s1=ggplot(data = econdata1,aes(x=theta,y=value))+geom_line(colour="blue")+
-        xlim(1.05,0.05)+
+        xlim(0.95,10.05)+
         facet_wrap(~measure)+theme(legend.position="")+
         geom_abline(slope = 0,intercept = 1,colour="red",linetype="dashed")+
         geom_abline(slope = 0,intercept = 0.05,colour="green",linetype="dashed")+
-        xlab("sigma")+ylab("Power")+
+        xlab("theta")+ylab("Power")+
         labs(title = "Comparison Of Power Curve in\n Parametric Case (F-Statistic)")+
        theme_mine() %+replace% theme(panel.spacing = unit(1, "lines"))
 
@@ -57,13 +57,13 @@ s2=ggplot(data = data1,aes(x=theta))+geom_line(aes(y=S_Exp,colour="Exp"))+
         scale_colour_manual(values =c("Exp"="green","Gamma"="brown",
                                       "Logis"= "blue","Normal"="red","Weibull"="orange"))+
         geom_abline(slope = 0,intercept = 1,colour="black",linetype="dashed")+
-        geom_abline(slope = 0,intercept = 0.05,colour="black",linetype="dashed")+xlim(1.05,0.05)+ylab("Power")+xlab("sigma")+
+        geom_abline(slope = 0,intercept = 0.05,colour="black",linetype="dashed")+xlim(0.95,10.05)+ylab("Power")+xlab("theta")+
         labs(title = "Comparison Of Power Curve in\n Parametric Case (F-Statistic)",colour="Colour")+
         theme_mine()%+replace%theme(legend.position=c(0.15,0.7))
 
 #Large_outer_true  Large_outer_False
 l1=ggplot(data = econdata,aes(x=theta2,y=value))+geom_line(colour="blue")+
-        xlim(1.05,0.15)+
+        xlim(0.95,5.05)+
         facet_wrap(~measure)+theme(legend.position="")+
         geom_abline(slope = 0,intercept = 1,colour="red",linetype="dashed")+
         geom_abline(slope = 0,intercept = 0.05,colour="green",linetype="dashed")+
@@ -81,8 +81,8 @@ l2=ggplot(data = data,aes(x=theta2))+geom_line(aes(y=L_Exp,colour="Exp"))+
         scale_colour_manual(values =c("Exp"="green","Gamma"="brown",
                                       "Logis"= "blue","Normal"="red","Weibull"="orange"))+
         geom_abline(slope = 0,intercept = 1,colour="black",linetype="dashed")+
-        geom_abline(slope = 0,intercept = 0.05,colour="black",linetype="dashed")+xlim(1.05,0.15)+ylab("Power")+xlab("theta")+
-        labs(title = "Comparison Of Power Curve in\n Parametric Case (F-Statistic)",colour="Colour")+
+        geom_abline(slope = 0,intercept = 0.05,colour="black",linetype="dashed")+xlim(0.95,5.05)+ylab("Power")+xlab("theta")+
+        labs(title = "Comparison Of Power Curve in\n Parametric Case Large Sample (F-Statistic)",colour="Colour")+
        theme_mine()%+replace%theme(legend.position=c(0.15,0.7))
 
 
